97f6f4238d04b37cd594b75a0eb3bb1e3707bb3f
Shivang Patel <patelshivang702@gmail.com>
2026-02-04 15:51:07 +0530

    initialized program enrollment manager service

---
+++ program-enrollment-manager/src/test/java/com/project/hems/program_enrollment_manager/ProgramEnrollmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hems.program_enrollment_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramEnrollmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
